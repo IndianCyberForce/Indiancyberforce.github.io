063f06e6e2922744dcd7bb924c8a772c9462db3e
Indian Cyber Force <BharatCyberForce@outlook.com>
2024-09-27 23:31:40 +0530

    Update donttrytostealourcode.js

--- GoogleHunter/donttrytostealourcode.js
+++ GoogleHunter/donttrytostealourcode.js
@@ -2,7 +2,7 @@
     var site = document.getElementById('target').value.trim();
 
     if (!site) {
-        alert("Enter your target site. (Ex:- site:indiancyberforce.in)");
+        alert("Enter your target site. (Ex:- site:indiancyberforce.github.io)");
         return;
     }
     var url = 'https://www.google.com/search?q=' + encodeURIComponent(site) + ' ';
@@ -63,3 +63,4 @@
 
     window.open(url, '_blank');
 }
+//TheIndainCyberForce
